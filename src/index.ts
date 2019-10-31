42581e3d56197b755ff9e04fcdfd7de84f607d64
Dean Radcliffe <deanmisc@yahoo.com>
2019-10-31 12:15:49 -0500

    useListener: brings event handling into your React component lifecycle.

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import {
+  agent as defaultAgent,
+  EventMatcher,
+  Subscriber,
+  HandlerConfig,
+} from 'rx-helper';
+
+/** useListener: Allows a component to attach (maybe async) consequences
+ * to event patterns: strings, regexes, or function predicates.
+ * Handlers return Observables for flexible scheduling/cancelation.
+ * Cleans up when the component is unmounted.
+ * @argument eventSpec - the string or regex to match the event type
+ * @argument handler - the function to be run (receiving {event})
+ * @argument options - If the handler closes over React hook variables which
+ * are not stable, provide the variables in the `deps` field of this object.
+ * To listen to an agent other than
+ */
+export const useListener = (
+  eventSpec: EventMatcher,
+  handler: Subscriber,
+  options: HandlerConfig = {}
+) => {
+  const { deps = [], agent = defaultAgent, ...config } = options;
+
+  useEffect(() => {
+    const subscription = agent.on(eventSpec, handler, config);
+
+    return () => subscription.unsubscribe();
+  }, deps);
+};
