336981ac64ea5246a63cc523fc2c54b96339ec98
Dean Radcliffe <deanmisc@yahoo.com>
2020-04-05 10:04:34 -0500

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,52 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext, createContext } from 'react';
 import {
-  agent as defaultAgent,
+  agent as defaultChannel,
   EventMatcher,
   Subscriber,
   HandlerConfig,
 } from 'rx-helper';
+
+// Call this anywhere in the tree to ensure that useListener/on, and trigger
+// are bound to this agent
+
+// Import ChannelContext and wrap YourComponent:
+//
+//  <ChannelContext.Provider value={yourChannel}>
+//    <YourComponent/>
+//  </ChannelContext.Provider>
+
+// In YourComponent:
+// const { trigger, on } = useChannel()
+export const ChannelContext = createContext(defaultChannel);
+
+export const useChannel = (deps = []) => {
+  const channel = useContext(ChannelContext) || defaultChannel;
+  return {
+    trigger(type: string, payload?: any) {
+      channel.trigger(type, payload);
+    },
+    on(
+      eventSpec: EventMatcher,
+      handler: Subscriber,
+      options: HandlerConfig = {}
+    ) {
+      useEffect(() => {
+        const sub = channel.on(eventSpec, handler, options);
+        return () => sub.unsubscribe();
+      }, deps);
+    },
+    filter(
+      eventSpec: EventMatcher,
+      handler: Subscriber,
+      options: HandlerConfig = {}
+    ) {
+      useEffect(() => {
+        const sub = channel.filter(eventSpec, handler, options);
+        return () => sub.unsubscribe();
+      }, deps);
+    },
+  };
+};
 
 /** useListener: Allows a component to attach (maybe async) consequences
  * to event patterns: strings, regexes, or function predicates.
@@ -14,17 +56,33 @@ import {
  * @argument handler - the function to be run (receiving {event})
  * @argument options - If the handler closes over React hook variables which
  * are not stable, provide the variables in the `deps` field of this object.
- * To listen to an agent other than
  */
 export const useListener = (
   eventSpec: EventMatcher,
   handler: Subscriber,
   options: HandlerConfig = {}
 ) => {
-  const { deps = [], agent = defaultAgent, ...config } = options;
+  const { deps = [], agent = defaultChannel, ...config } = options;
 
   useEffect(() => {
     const subscription = agent.on(eventSpec, handler, config);
+
+    return () => subscription.unsubscribe();
+  }, deps);
+};
+
+/** useListener: Allows a component to intercept and run synchronous
+ consequences, alter events, or throw errors to cancel the processing
+*/
+export const useFilter = (
+  eventSpec: EventMatcher,
+  handler: Subscriber,
+  options: HandlerConfig = {}
+) => {
+  const { deps = [], agent = defaultChannel, ...config } = options;
+
+  useEffect(() => {
+    const subscription = agent.filter(eventSpec, handler, config);
 
     return () => subscription.unsubscribe();
   }, deps);
@@ -46,5 +104,5 @@ export const useEffectAfterMount = (
   }, deps);
 };
 
-export const channel = defaultAgent;
+export const channel = defaultChannel;
 export * from 'rx-helper';
