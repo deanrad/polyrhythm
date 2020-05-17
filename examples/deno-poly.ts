da3f67aa7c7c7289fa4c2f6f1f483323a14b4ba3
Dean Radcliffe <deanmisc@yahoo.com>
2020-05-16 23:41:30 -0500

    Added deno usage example
    Fixes #10

---
+++ examples/deno-poly.ts
@@ -0,0 +1,14 @@
+/*
+A basic version showing how to execute evented polyrhythm-style
+code inside the new runtime DENO.
+
+> deno run --allow-net=githubusercontent.com examples/deno-poly.ts
+*/
+import {
+  channel,
+  after,
+} from 'https://raw.githubusercontent.com/deanius/monolog/master/polyrhythm.1.0.0.development.js';
+channel.listen('greet', () => console.log('World'));
+channel.filter(true, () => console.log('Hello'));
+channel.trigger('greet');
+await after(1000, () => console.log('Goodbye!'));
